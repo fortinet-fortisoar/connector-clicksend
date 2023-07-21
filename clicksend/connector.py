c788d7bba65a9f2edc5ed3ba036575710ff27098
usurana <usurana@fortinet.com>
2023-07-21 10:56:13 +0530

    Handled check health error and added new actions and parameters

--- clicksend/connector.py
+++ clicksend/connector.py
@@ -1,38 +1,30 @@
-from connectors.core.connector import Connector
-from connectors.core.connector import get_logger, ConnectorError
-from .operations import operations
-from django.core.mail import send_mail, get_connection
+""" Copyright start
+  Copyright (C) 2008 - 2023 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end """
+
+from connectors.core.connector import Connector, get_logger, ConnectorError
+from .operations import operations, _check_health
 
 logger = get_logger('clicksend')
 
 
 class Clicksend(Connector):
 
-    def on_add_config(self, config, active):
-        try:
-            tokens = operations._get_access_token(config)
-            config["access_token"] = tokens["access_token"]
-            config["refresh_token"] = tokens["refresh_token"]
-        except Exception as e:
-            error_message = "Error setting access and refresh tokens. Error message as follows:\n{}".format(str(e))
-            logger.exception(error_message)
-            raise ConnectorError(error_message)
-
     def execute(self, config, operation, params, **kwargs):
         try:
-            action = operations.get(operation)
-            result = {}
+            operation = operations.get(operation)
+            if not operation:
+                logger.error('Unsupported operation: {}'.format(operation))
+                raise ConnectorError('Unsupported operation')
+            return operation(config, params)
+        except Exception as err:
+            logger.exception(err)
+            raise ConnectorError(err)
 
-            result = action(config, params)
-            return result
-        except Exception as e:
-            error_message = "Error in execute function. Error message as follows:\n{}".format(str(e))
-            raise ConnectorError(error_message)
-
-
-    def check_health(self, config):
+    def check_health(self, config=None):
         try:
-            return operations.get("check_health")(config)
-        except Exception as e:
-            error_message = "Error in health check. Error message as follows:\n{}".format(str(e))
-            raise ConnectorError(error_message)
+            return _check_health(config)
+        except Exception as err:
+            raise ConnectorError(err)
---
+++ clicksend/release_notes.md
@@ -0,0 +1,15 @@
+#### What's Improved
+
+- Added following new parameters in the action `Send Message`
+  - Custom String
+  - Country
+  - List ID
+  - Email
+
+- Added following new operations and respective playbooks
+  - Get Host Details
+  - Get Scan History
+
+#### What's Fixed
+
+- Fixed the connector issue where check health was failing because of the following error - `'cannot import name '_api_call'`
